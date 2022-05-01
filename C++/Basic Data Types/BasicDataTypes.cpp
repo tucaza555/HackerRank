840d201e741b90be730d4556dead103e7810d499
tucaza555 <68226299+tucaza555@users.noreply.github.com>
2022-05-01 18:45:49 +1000

    Create BasicDataTypes.cpp

---
+++ C++/Basic Data Types/BasicDataTypes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    scanf("%d", &a);
+    scanf("%ld", &b);
+    scanf(" %c", &c);
+    scanf("%f", &d);
+    scanf("%lf", &e);
+    
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", c);
+    printf("%.3f\n", d);
+    printf("%lf", e);
+    
+    
+    return 0;
+}

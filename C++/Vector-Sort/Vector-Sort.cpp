76dd98677413ff2011357e8055852a2b653afb26
tucaza555 <68226299+tucaza555@users.noreply.github.com>
2022-05-02 17:35:44 +1000

    Create Vector-Sort.cpp

---
+++ C++/Vector-Sort/Vector-Sort.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() { 
+    vector<int> v;
+    int size; 
+    
+    cin >> size;
+    
+    for(int i = 0; i < size; i++) {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    
+    sort(v.begin(), v.end());
+    
+    for(int i = 0; i < size; i++) {
+        cout << v[i] << " ";
+    }
+    return 0;
+}

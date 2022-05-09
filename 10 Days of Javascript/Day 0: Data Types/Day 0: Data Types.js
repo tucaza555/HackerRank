ddc13f92dd8a47f8335598a02591c7a33ae8c539
Zac <68226299+tucaza555@users.noreply.github.com>
2022-05-10 00:25:15 +1000

    Update Day 0: Data Types.js

--- 10 Days of Javascript/Day 0: Data Types/Day 0: Data Types.js
+++ 10 Days of Javascript/Day 0: Data Types/Day 0: Data Types.js
@@ -45,13 +45,13 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     const firstString = 'HackerRank ';
     
     // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
-    
+    console.log(parseInt(firstInteger) + parseInt(secondInteger));
     
     // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
-    
+    console.log(parseFloat(firstDecimal) + parseFloat(secondDecimal));
     
     // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
-    
+    console.log(firstString + secondString);
 }
 
 
